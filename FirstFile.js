c320d96ca2bb72f505449e883cd63184ad9f6423
LaiGiaBao <baolaigia110501@gmail.com>
2023-01-11 11:33:46 -0500

    Second commit

--- FirstFile.js
+++ FirstFile.js
@@ -1,3 +1,12 @@
-for (let idx = 0; idx < 10000; idx++) {
-  console.log(idx);
+//core javascript/ vanilla js
+//variable declaration
+var sessionName = "MERNStack Session",
+  month = "January";
+console.log("session Name is - ", sessionName + " " + month);
+//function keyword
+function PrintSession(sessionName) {
+  console.log("Session Name from function - ", sessionName);
 }
+// Area are = new Area();
+// Area.printRectangle();
+PrintSession("Core Javascript");
