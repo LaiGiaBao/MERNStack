a747c929260c5053987d077494d28dc76013137b
LaiGiaBao <baolaigia110501@gmail.com>
2023-01-11 12:24:12 -0500

    Third COmmit

---
+++ DataTypes.js
@@ -0,0 +1,9 @@
+console.log(SomeValue);
+var SomeValue = "MERNStack Trainees";
+
+console.log(SomeValue);
+console.log(typeof SomeValue);
+
+var SomeValue = 232322302392034823452097;
+console.log(SomeValue);
+console.log(typeof SomeValue);
--- FirstFile.js
+++ FirstFile.js
@@ -2,11 +2,18 @@
 //variable declaration
 var sessionName = "MERNStack Session",
   month = "January";
-console.log("session Name is - ", sessionName + " " + month);
+// console.log("session Name is - ", sessionName + " " + month);
 //function keyword
 function PrintSession(sessionName) {
   console.log("Session Name from function - ", sessionName);
 }
 // Area are = new Area();
 // Area.printRectangle();
-PrintSession("Core Javascript");
+// PrintSession("Core Javascript");
+
+//Name
+var myName = "Gia B. Lai";
+function printName(name) {
+  console.log("My name is", myName);
+}
+printName(myName);
